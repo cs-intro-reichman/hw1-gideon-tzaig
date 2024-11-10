f8bab26173007d72bf3ac43a471909c779d82cb4
Gideon Tzaig <gideon.tzaig@post.runi.ac.il>
2024-11-10 17:13:02 +0200

    FVCalc bug fix - ceil and a typo

--- FVCalc.java
+++ FVCalc.java
@@ -1,11 +1,11 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		// Calculator which predict future value of ventures
 		int currentValue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		double numberOfYears = Double.parseDouble(args[2]);
-		double futureValue = Math.round(currentValue * Math.pow((1 + rate/100), numberOfYears));
+		double futureValue = Math.ceil(currentValue * Math.pow((1 + rate/100), numberOfYears));
 		System.out.printf("%n After %.0f years, $%d saved at %.1f%% will yeild a $%d",
 		numberOfYears, currentValue, rate, (int) futureValue);
 	}
