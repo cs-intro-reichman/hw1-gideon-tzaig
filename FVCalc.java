f1b57d1e464f3934e6d0940c58b6c13b80ece289
Gideon Tzaig <gideon.tzaig@post.runi.ac.il>
2024-11-10 14:14:05 +0200

    FVCalc bug fix - % sing & calc func

--- FVCalc.java
+++ FVCalc.java
@@ -3,10 +3,10 @@ public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
 		int currentValue = Integer.parseInt(args[0]);
-		double rate = Math.ceil(Double.parseDouble(args[1]) / 100);
+		double rate = Double.parseDouble(args[1]);
 		double numberOfYears = Double.parseDouble(args[2]);
-		double futureValue = currentValue * Math.pow((1 + rate), numberOfYears);
-		System.out.printf("%n After %s years, $%s saved at %s.0% will yeild $%s",
-			numberOfYears, currentValue, rate, (int) futureValue);
+		double futureValue = Math.round(currentValue * Math.pow((1 + rate/100), numberOfYears));
+		System.out.printf("%n After %.0f years, $%d saved at %.1f%% will yeild $%d",
+		numberOfYears, currentValue, rate, (int) futureValue);
 	}
 }
