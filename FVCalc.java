558f2f3d8ca49c0dd9153a8f64063ed5e2d9b519
Gideon Tzaig <gideon.tzaig@post.runi.ac.il>
2024-11-14 13:34:06 +0200

    FVCalc bug fix - change double to integer

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,8 @@ public class FVCalc {
 		// Calculator which predict future value of ventures.
 		int currentValue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);
-		double numberOfYears = Double.parseDouble(args[2]);
-		double futureValue = currentValue * Math.round(Math.pow((1 + rate/100), numberOfYears));
+		int numberOfYears = Integer.parseInt(args[2]);
+		double futureValue = Math.round(currentValue * Math.pow((1 + rate/100), numberOfYears));
 		System.out.printf("After %.0f years, a $%d saved at %.1f%% will yield $%d", 
 		numberOfYears, currentValue, rate, (int) futureValue);
 	}
