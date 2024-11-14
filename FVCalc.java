3cabbf51034310c0d31dc2a0d024490a61fdb657
Gideon Tzaig <gideon.tzaig@post.runi.ac.il>
2024-11-14 13:55:03 +0200

    FVCalc bug fix - printf -> print

--- FVCalc.java
+++ FVCalc.java
@@ -5,8 +5,8 @@ public class FVCalc {
 		int currentValue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int numberOfYears = Integer.parseInt(args[2]);
-		double futureValue = Math.round(currentValue * Math.pow((1 + rate/100), numberOfYears));
-		System.out.printf("After " + numberOfYears + " years, a $" + currentValue + " saved at " + rate + "%" + " will yield $" + (int) futureValue
+		double futureValue = currentValue * Math.pow((1 + rate/100), numberOfYears);
+		System.out.print("After " + numberOfYears + " years, a $" + currentValue + " saved at " + rate + "%" + " will yield $" + (int) futureValue
 		);
 	}
 }
